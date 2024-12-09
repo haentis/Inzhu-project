5f03ffcc913b575ad16d148c19659f7a2e7e8760
Iskander <135708220+haentis@users.noreply.github.com>
2024-12-09 16:27:32 +0500

    )))

--- beauty_app/src/components/Reviews.jsx
+++ beauty_app/src/components/Reviews.jsx
@@ -1,31 +1,77 @@
-import menImage from '../assets/reviewsImage.png';
+// import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import firstRev from '../assets/firstRev.jpg';
+import secondRev from '../assets/secondRev.jpg';
+import lastRev from '../assets/lastRev.jpg';
+
 import reviewsImage2 from '../assets/reviewsImages2.png';
 
-function Reviews(){
-    return(
-        <>
-            <div className="w-full h-[1000px]  flex justify-center items-center mt-16" style={{ userSelect: "none" }}>
-                <div className="w-4/5 h-[500px] bg-white  flex flex-row gap-40">
-                    <img src={menImage} alt=""  className=''/>
-                    <div className='w-[500px] h-[500px] '>
-                        <img src={reviewsImage2} alt="" />
-                        <p className='text-3xl pt-11'>
-                            Amet minim mollit non deserunt <br />
-                            est sit aliqua dolor do amet sint <br />
-                            officia consequat duis enim mollit <br />
-                            exercitation.
-                        </p>
-                        <img src={reviewsImage2} alt="" className='pt-10' />
-                        <p className='pt-14'>Lee Morgan, Slack, Inc</p>
-                        <p className=''>New York, NY Team</p>
-                    </div>
-                </div>
-               
+function Reviews() {
+  return (
+    <div
+      className="w-full h-[1000px] flex justify-center items-center mt-16"
+      style={{ userSelect: 'none' }}
+    >
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={50}
+        slidesPerView={1}
+        className="w-4/5 h-[500px] bg-white"
+      >
+        {/* Первый слайд */}
+        <SwiperSlide>
+          <div className="flex flex-row gap-40">
+            <img src={firstRev} alt="Review 1" className="w-[400px]" />
+            <div className="w-[500px] h-[500px]">
+              <img src={reviewsImage2} alt="Review Icon 1" />
+              <p className="text-3xl pt-11">
+              Сыворотка өте күшті, теріні жақсы ылғалдандырады. <br />
+              Тез сіңіп кетеді, әрі бетімді майлы ғылдырып турмайды🔥
+              </p>
+              <img src={reviewsImage2} alt="Reviewer" className="pt-10" />
             </div>
-        </>
-    )
+          </div>
+        </SwiperSlide>
+
+        {/* Второй слайд */}
+        <SwiperSlide>
+            <div className="flex flex-row gap-40">
+                <img src={secondRev} alt="Review 1" className="w-[400px]" />
+                <div className="w-[500px] h-[500px]">
+                <img src={reviewsImage2} alt="Review Icon 1"/>
+                <p className="text-3xl pt-11">
+                    Осы продукцианы жиі аламын, spf кремі өте күшті, кунен теріні жақсы қорғайды, әрі ылғалдандырады. <br />
+                    Осы spf жеңіл жағылады, әрі тез сіңіп кетеді, <br />
+                    жаққанан кейін эффекті 2 реттен байкадым, маған ұнады😍 
+                </p>
+                <img src={reviewsImage2} alt="Reviewer" className="pt-10" />
+                </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="flex flex-row gap-40">
+                <img src={lastRev} alt="Review 1" className="w-[400px]" />
+                <div className="w-[500px] h-[500px]">
+                <img src={reviewsImage2} alt="Review Icon 1" />
+                <p className="text-3xl pt-11">
+                d’alba гельын бырыншы рет алып кордым. Байкаганым оте жаксы тазартады, <br />
+                терыны катты кептырмейды екен. Макияжды барын тап таза жуып тастайды.  <br />
+                Терыге питание береды.👍
+                </p>
+                <img src={reviewsImage2} alt="Reviewer" className="pt-10" />
+                </div>
+            </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
-
-
 
 export default Reviews;
